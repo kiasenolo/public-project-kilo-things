c1ac004c487787143f2b73a37a10fb45cc5a939a
KIASENOLO <71315290+kilo9487@users.noreply.github.com>
2026-05-11 23:18:01 +0800

    把一些東西的cfg補上去好了

---
+++ data/other/DefaultConfig/components/FileDrop.ts
@@ -0,0 +1,13 @@
+export type CfgType = {
+  DisplayCount: boolean;
+  DisplayTips: boolean;
+  BackdropBlur: boolean;
+}
+
+const FileDropCfg = {
+  DisplayCount: true,
+  DisplayTips: true,
+  BackdropBlur: true,
+}
+
+export default FileDropCfg
---
+++ data/other/DefaultConfig/pages/AFK.ts
@@ -0,0 +1,15 @@
+export type CfgType = {
+  DisplayCustomText: boolean;
+  CustomTextCanEdit: boolean;
+  BatteryAmountDisplay: boolean;
+  ChargingAnimation: boolean;
+}
+
+const AFKCfg: CfgType = {
+  DisplayCustomText: true,
+  CustomTextCanEdit: true,
+  BatteryAmountDisplay: true,
+  ChargingAnimation: true,
+}
+
+export default AFKCfg
